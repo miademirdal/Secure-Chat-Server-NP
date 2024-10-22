59d5d155412b9e5cd0b3b47460854ec4ae1ae22d
miademirdal <miademirdal@gmail.com>
2024-10-22 14:28:11 -0400

    Wrapping Server with TLS encryption.
    Revising and updating ServerSocket with TLS encryption and a skeleton for what messages the Server will send back to the client.

--- Server/ServerSocket.py
+++ Server/ServerSocket.py
@@ -1,4 +1,6 @@
 import socket
+import ssl
+import threading
 
 class ServerSocket:
     """server class"""
@@ -7,39 +9,46 @@ class ServerSocket:
         self.host = host
         self.port = port
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen()
+        print(f"Server started on {self.host}:{self.port}")
+        
+        #ssl content
+        self.context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        self.context.load_cert_chain(certfile='server.crt', keyfile='server.key')
+    
+    def ClientConnect(self, client_socket):
+        print(f"Client Connected")
+        while True:
+            try:
+                data = client_socket.recv(1024)
+                if not data:
+                    break;
+                print(f"Received from client: {data.decode('utf-8')}")
+                responseMessage = "Server: Message received!"
+                client_socket.sendall(responseMessage.encode('utf-8'))
+                    
+                #client_socket, addr = self.server_socket.accept()
+                
+                #print(f"got connected on {addr}")
+                #for message in messages:
+                #    client_socket.send(message.encode('utf-8'))
+                
+            except Exception as e:
+                print(f"An error occured: {e}")
+                break
     
     def start_server(self):
         print("Starting Server...")
-        try:
-            self.server_socket.bind((self.host, self.port))
-            self.server_socket.listen(2)
-        
-            print(f"server connected on {self.host}:{self.port}")
-        
-            while True:
-                client_socket, addr = self.server_socket.accept()
-                print(f"got connected on {addr}")
+        while True:
+            client_socket, addr = self.server_socket.accept()
+            print(f"Got connected from {addr}")
             
-                messages = [
-                    "Server: Hello~~",
-                    "Server: What's up",
-                    "Server: Your chat program works well~",
-                    "Server: Ok. Have fun!"
-                ]
-
-                for message in messages:
-                    client_socket.send(message.encode('utf-8'))
-                    data = client_socket.recv(1024)
-                
-                if data:
-                    print(f"Received from client: {data.decode('utf-8')}")
-
-        except Exception as e:
-            print(f"An error occured: {e}")
-
-        finally:
-            client_socket.close()
-    
+            #Wrap the client socket with TLS
+            secure_client_socket = self.context.wrap_socket(client_socket, server_side=True)
+            client_thread = threading.Thread(target=self.handle_client, args=(secure_client_socket,))
+            client_thread.start()
+            
 if __name__ == "__main__":
     host = '127.0.0.1'
     port = 1200
